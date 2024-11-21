9d499621f1907b7ee59b118df888cce668bca919
Wambu-i7 <wambuindikiru@gmail.com>
2024-11-21 07:21:49 -0800

    nesting

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,19 +1,9 @@
-import { Map } from 'immutable';
-
 export default function accessImmutableObject(object, array) {
-  const result = array.reduce((acc, key) => (acc ? acc[key] : undefined), object);
-
-  if (result === undefined) {
-    return undefined;
-  }
-
-  if (typeof result === 'string') {
+  const result = array.reduce((acc, key) => acc && acc[key], object);
+  
+  if (result === undefined || typeof result === 'string' || result instanceof Map) {
     return result;
   }
-
-  if (result instanceof Map) {
-    return result;
-  }
-
+  
   return undefined;
 }
