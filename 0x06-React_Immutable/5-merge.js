e58b85290f0140acd45fc184c0f7814c433d08a3
Wambu-i7 <wambuindikiru@gmail.com>
2024-11-21 07:47:12 -0800

    merge

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,17 +1,14 @@
 const { List, Map } = require('immutable');
 
-// Function to concatenate two arrays into an immutable List
 function concatElements(page1, page2) {
-    return List(page1.concat(page2)); // Concatenates the two arrays and converts them to an immutable List
+    return List(page1.concat(page2));
 }
 
-// Function to merge two objects into an immutable List, with page2's values overriding page1's for duplicates
 function mergeElements(page1, page2) {
     const mergedMap = Map(page1).mergeWith((v1, v2) => v2, Map(page2));
-    return List(mergedMap.values()); // Convert the merged map values to a List
+    return List(mergedMap.values());
 }
 
-// Export the functions to be used in other modules
 module.exports = {
     concatElements,
     mergeElements
