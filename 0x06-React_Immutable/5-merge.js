32a1eaa609dd02489c23ac13f6972d613a1fba01
Wambu-i7 <wambuindikiru@gmail.com>
2024-11-22 08:04:22 -0800

    Merge

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,9 +1,15 @@
-import { List } from 'immutable';
+import { List, Map } from 'immutable';
 
-export function getListObject(array) {
-    return List(array);
+function concatElements(page1, page2) {
+    return List.of(...page1, ...page2);
 }
 
-export function addElementToList(list, element) {
-    return list.push(element);
+function mergeElements(page1, page2) {
+    const mergedMap = Map(page1).merge(Map(page2));
+    return List(mergedMap.values());
 }
+
+export {
+    concatElements,
+    mergeElements
+};
