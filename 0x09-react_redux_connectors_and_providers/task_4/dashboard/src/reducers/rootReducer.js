7e89751071d1900b861662c5531f0d5b5a432aac
Wambu-i7 <wambuindikiru@gmail.com>
2024-12-07 01:28:43 -0800

    root reducers

---
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/reducers/rootReducer.js
@@ -0,0 +1,14 @@
+// src/reducers/rootReducer.js
+import { combineReducers } from 'redux';
+import courseReducer from './courseReducer';
+import notificationReducer from './notificationReducer';
+import uiReducer from './uiReducer';
+
+const rootReducer = combineReducers({
+  courses: courseReducer,
+  notifications: notificationReducer,
+  ui: uiReducer,
+});
+
+export default rootReducer;
+
